326ee5793bcf5492b8cc0ec2d6e2d4d022416e14
serblowup <serg2005savchenko@gmail.com>
2024-12-22 14:18:00 +0300

    index.js

--- Game/index.js
+++ Game/index.js
@@ -2,16 +2,16 @@ kaboom();
 
 const WIDTH = width();
 const HEIGHT = height();
-const FLOOR_HEIGHT = HEIGHT / 2;
+const FLOOR_HEIGHT = HEIGHT / 4;
 const JUMP_FORCE = 900;
 const SPEED = 500;
-const BACKGROUND_COLOR = Color.fromHex("#8D87FF");
-const GRASS_COLOR = Color.fromHex("#163301");
-const FLOOR_COLOR = Color.fromHex("#009A17");
+const BACKGROUND_COLOR = Color.fromHex("#6495ED");
+const GRASS_COLOR = Color.fromHex("#b3bfae");
+const FLOOR_COLOR = Color.fromHex("#c9d2c6");
 
 setBackground(BACKGROUND_COLOR);
-loadSprite("IGROK", "car.png");
-loadSprite("PREPYATSTVIE", "block.png");
+
+loadSprite("block", "block.png");
 
 const playerSprites = ["P1", "P2", "P3", "P4", "P5","P6", "P7", "P8", "P9", "P10", "P11"];
 playerSprites.forEach(spriteName => loadSprite(spriteName, `pl${spriteName.slice(1)}.png`));
@@ -20,7 +20,7 @@ const deathSprites = ["D1", "D2", "D3", "D4", "D5"];
 deathSprites.forEach(spriteName => loadSprite(spriteName, `d${spriteName.slice(1)}.png`));
 
 scene("MainMenu", function () {
-    loadSprite("BackgroundMainMenu", "Background1.jpg");
+    loadSprite("BackgroundMainMenu", "BackgroundMenu.jpg");
 
     const scaleX = WIDTH / 873;
     const scaleY = HEIGHT / 528;
@@ -34,8 +34,19 @@ scene("MainMenu", function () {
         anchor("center")
     ]);
 
-    const Menu = add([text("Главное меню."), pos(700, 50), scale(2), anchor("center")]);
-    const Start = add([text("Начать игру."), pos(700, 300), scale(2), anchor("center"), area()]);
+    const Menu = add([
+        text("Главное меню."),
+        pos(700, 50),
+        scale(2), 
+        anchor("center")]);
+
+    const Start = add([
+        text("Начать игру."), 
+        pos(700, 300), 
+        scale(2), 
+        anchor("center"), 
+        area()]);
+
     Start.onClick(function () {
         go("game", 0);
     });
@@ -45,7 +56,13 @@ scene("game", function (InitialScore, playerState, blocksState) {
     setGravity(2000);
 
     const createPlayer = function (state) {
-        const player = add([sprite(playerSprites[0]), scale(1), pos(80, 40), area(), body(), { currentFrame: 0, isPaused: false }]);
+        const player = add([
+            sprite(playerSprites[0]),
+            scale(1),
+            pos(80, HEIGHT - FLOOR_HEIGHT - 40),
+            area(),
+            body(),
+            { currentFrame: 0, isPaused: false }]);
         if (state) {
             player.pos = state.pos;
             player.vel = state.vel;
@@ -56,7 +73,14 @@ scene("game", function (InitialScore, playerState, blocksState) {
     const player = createPlayer(playerState);
 
     const createFloor = function () {
-        add([rect(WIDTH, FLOOR_HEIGHT), outline(4, GRASS_COLOR), color(FLOOR_COLOR), pos(0, HEIGHT), anchor("botleft"), area(), body({ isStatic: true })]);
+        add([
+            rect(WIDTH, FLOOR_HEIGHT),
+            outline(4, GRASS_COLOR),
+            color(FLOOR_COLOR),
+            pos(0, HEIGHT),
+            anchor("botleft"),
+            area(),
+            body({ isStatic: true })]);
     }
 
     createFloor();
@@ -67,14 +91,30 @@ scene("game", function (InitialScore, playerState, blocksState) {
 
     const spawnBlock = function () {
         if (!shouldSpawnBlocks) return;
-        const block = add([sprite("PREPYATSTVIE"), scale(0.6), area(), pos(WIDTH, FLOOR_HEIGHT), anchor("botleft"), move(LEFT, SPEED), offscreen({ destroy: true }), "block"]);
+        const block = add([
+            sprite("block"),
+            scale(0.6),
+            area(),
+            pos(WIDTH, HEIGHT - FLOOR_HEIGHT),
+            anchor("botleft"),
+            move(LEFT, SPEED),
+            offscreen({ destroy: true }),
+            "block"]);
         blocks.push(block);
         wait(rand(1, 3), spawnBlock);
     }
 
     if (blocksState) {
         blocksState.forEach(blockState => {
-            const block = add([sprite("PREPYATSTVIE"), scale(0.6), area(), pos(blockState.pos), anchor("botleft"), move(LEFT, SPEED), offscreen({ destroy: true }), "block"]);
+            const block = add([
+                sprite("block"),
+                scale(0.6),
+                area(),
+                pos(blockState.pos),
+                anchor("botleft"),
+                move(LEFT, SPEED),
+                offscreen({ destroy: true }),
+                "block"]);
             blocks.push(block);
         });
         wait(rand(1, 3), spawnBlock);
@@ -112,7 +152,10 @@ scene("game", function (InitialScore, playerState, blocksState) {
     onClick(jump);
 
     const createScoreLabel = function () {
-        const scoreLabel = add([text(score), scale(1), pos(25, 25)]);
+        const scoreLabel = add([
+            text(score), 
+            scale(1), 
+            pos(25, 25)]);
         return scoreLabel;
     }
 
@@ -145,9 +188,24 @@ scene("game", function (InitialScore, playerState, blocksState) {
 });
 
 scene("Pause", (score, playerState, blocksState) => {
-    const PauseText = add([text("Пауза."), pos(700, 50), scale(2), anchor("center")]);
-    const Resume = add([text("Продолжить."), pos(700, 200), scale(2), anchor("center"), area()]);
-    const Back = add([text("Выйти."), pos(700, 400), scale(2), anchor("center"), area()]);
+    const PauseText = add([
+        text("Пауза."), 
+        pos(700, 50), 
+        scale(2), 
+        anchor("center")]);
+
+    const Resume = add([
+        text("Продолжить."), 
+        pos(700, 200), 
+        scale(2), 
+        anchor("center"), 
+        area()]);
+    const Back = add([
+        text("Выйти."), 
+        pos(700, 400), 
+        scale(2), 
+        anchor("center"), 
+        area()]);
 
     Resume.onClick(function () {
         go("game", score, playerState, blocksState);
@@ -158,11 +216,21 @@ scene("Pause", (score, playerState, blocksState) => {
     });
 });
 
-scene("lose", (score) => {
-    add([text("Игра закончена."), pos(center()), scale(2), anchor("center")]);
-    add([text("Счёт: " + score), pos(WIDTH / 2, HEIGHT / 2 + 64), scale(2), anchor("center")]);
+scene("lose", function(score) {
+    add([
+        text("Игра закончена."), 
+        pos(center()), 
+        scale(2), 
+        anchor("center")]);
+
+    add([
+        text("Счёт: " + score), 
+        pos(WIDTH / 2, HEIGHT / 2 + 64), 
+        scale(2), 
+        anchor("center")]);
+        
     onClick(function () {
-        go("game", 0);
+        go("MainMenu");
     });
 });
 
